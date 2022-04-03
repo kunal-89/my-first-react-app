8a330d7c2b603b26e34fc9a98b2a755910580734
kunal-89 <kunalbherokhra@gmail.com>
2022-04-03 20:57:07 +0530

    Firebase deploy.

--- src/container/Subtraction.js
+++ src/container/Subtraction.js
@@ -3,7 +3,6 @@ import SubtractionComponent from '../components/subtraction/Subtraction';
 import { subtract } from '../store/actions';
 const Subtraction = () => {
     const minus = useSelector((state) => state.substraction.minus);
-    debugger;
     const dispatch = useDispatch()
     return (
         <SubtractionComponent subtract={subtract} minus={minus} dispatch={dispatch}/>
